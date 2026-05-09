ce84298f0e9ae943a6710980147e618780d51753
jvskinner <j.v.skinner.jr@gmail.com>
2026-05-09 08:36:45 -0500

    update from windows

--- tests/loadViewer.spec.ts
+++ tests/loadViewer.spec.ts
@@ -3,21 +3,37 @@ import { selStudyDatesCalendar, selectStudy} from "./utils";
 
 test('test', async ({ page }) => {
     let userNumber=5;
-    test.setTimeout(300000);
-    const userNumberStr = process.env.USER_NUMBER
-    userNumber = Number(userNumberStr);
-    let userName='tomea' + userNumber.toString();
+    test.setTimeout(300000000);
+
     await page.goto('https://webviz.xyz');
     await page.waitForTimeout(1000);
+    page.on('console',(msg)=> {
+        console.log(msg);
+    })
     await page.getByRole('textbox', { name: 'Username' }).click();
     await page.getByRole('textbox', { name: 'Username' }).fill('king');
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('King@2026');
     await page.getByRole('textbox', { name: 'Password' }).press('Enter');
     await selStudyDatesCalendar(page,4,3);
-    for (let i = 0;i < 10;i++) {
+
+    for (let i = 0;i < 10000;i++) {
 
         await selectStudy(page,'RAMULLAMMA',false);
+        await page.waitForTimeout(2000);
+        await page.locator('#iFrameDiv iframe').contentFrame().locator('div:nth-child(7) > div > div > .relative > .text-center').click();
+        await page.waitForTimeout(2000);
+
+        await page.locator('#iFrameDiv iframe').contentFrame().locator('div').filter({ hasText: /^MPR$/ }).first().click();
+        for (let j=0;j< 10;j++) {
+            await page.waitForTimeout(1000);
+            await page.locator('#iFrameDiv iframe').contentFrame().locator('body').press('ArrowUp');
+        }
+
+
+
+        console.log(`iteration: ${i}`)
+
         await page.getByRole('button', { name: 'file x' }).click();
     }
 
